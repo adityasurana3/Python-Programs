4093dad42322e3ce8cf8994c4c6fe91bd298997d
adityasurana3 <suranaaditya3@gmail.com>
2021-01-26 10:59:13 +0530

    OOPS Done

--- oops.py
+++ oops.py
@@ -1,9 +1,87 @@
-class Form:
-    def printData(self):
-        print(f"My name is {self.name}")
-        print(f"last name is {self.last_name}")
+# class Player():
+#     def moveLeft(self):
+#         print("Left moved")
 
-form1=Form()
-form1.name="Aditya"
-form1.last_name="Surana"
-form1.printData()
+#     def moveRight(self):
+#         print("Right moved")
+
+#     def moveUp(self):
+#         print("Up moved")
+
+#     def moveDown(self):
+#         print("Down moved")
+
+
+# player1 = Player()
+# player1.moveLeft()
+# player1.moveRight()
+# player1.moveRight()
+# player1.moveDown()
+
+
+# class Company():
+#     company="Google"
+#     def printData(self):
+#         print(self.aditya)
+# aditya=Company()
+# print(aditya.company)
+# # aditya.printData()
+# Company.company="Microsoft"
+
+# class Employee():
+#     def printData(self):
+#         print(f"salary is {self.salary}")
+
+# aditya= Employee()
+# harry= Employee()
+# aditya.salary = "100k"
+# harry.salary = "100k"
+# # ==>Employee.printData(aditya)
+# aditya.printData() 
+# # ==>Employee.printData(harry)
+# harry.printData()
+
+
+# class Programmer():
+#     def info(self):
+#         print(f"Name: {self.name}")
+#         print(f"salary: {self.salary}")
+
+# aditya=Programmer()
+# person1=Programmer()
+# aditya.name=input("enter your name")
+# person1.name=input("enter your name")
+# aditya.salary=input("enter your salary")
+# person1.salary=input("enter your salary")
+# aditya.info()
+# person1.info()
+
+# class Calculator:
+#     def __init__(self, num):
+#         print("Constructed")
+#         self.number = num
+#         # self.name = name
+    
+#     def squarecube(self):
+#         print(f"square cube of{self.number} is {self.number**3}")
+#     def squareroot(self):
+#         print(f"square cube of{self.number} is {self.number**0.5}")
+#     @staticmethod
+#     def greet(name):
+#         print(f"hello {name}")
+
+# num=int(input("Enter the number"))
+# name=input("Enter your name")
+# print("hello1")
+# cal = Calculator(num)
+# print("hello2")
+# cal.squarecube()
+# cal.squareroot()
+# cal.greet(name)
+
+
+# class attribute:
+#     a=1
+#     print(a)
+# att = attribute()
+# att.a=0
